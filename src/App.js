a4a27863c3e82e007864af989965a63e1a85b7a4
Brenda Moura <95611970+brendamoura1997@users.noreply.github.com>
2026-06-19 15:45:50 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Contact from "./components/sections/Contact";
 import Footer from "./components/layout/Footer";
 import { useScreenSize } from "./hooks/useDevice";
 import { theme } from "./styles/theme";
-import GlobalStyle from "./styles/GlobalStyle";
+import GlobalStyle from "./styles/globalStyle";
 import ResetStyle from "./styles/resetStyle";
 
 const Container = styled.div`
